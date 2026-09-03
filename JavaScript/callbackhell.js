6679b0f70c5b9b4fce5e6602d209052a71bc00d4
tanmay <gargtanmay65@gmail.com>
2026-09-03 14:13:11 +0530

    callback

---
+++ JavaScript/callbackhell.js
@@ -0,0 +1,31 @@
+function test1(cb){
+    setTimeout(()=>{
+        console.log("Task1");
+        cb();
+    },4000);
+}
+function test2(cb){
+    setTimeout(()=>{
+        console.log("Task2");
+        cb();
+    },2000);
+}
+function test3(cb){
+    setTimeout(()=>{
+        console.log("Task3");
+        cb();
+    },200);
+}
+function test4(cb){
+    setTimeout(()=>{
+        console.log("Task4");
+        cb();
+    },100);
+}
+test1(()=>{
+    test2(()=>{
+        test3(()=>{
+            test4();
+        });
+    });
+});
