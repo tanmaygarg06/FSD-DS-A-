37ecbc39b41b2be27eb502e803d265ead5739fd8
tanmay <gargtanmay65@gmail.com>
2026-09-10 13:33:15 +0530

    async-await

---
+++ JavaScript/promise.js
@@ -0,0 +1,32 @@
+function f1(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+        console.log("Hello");
+        resolve();
+        },4000);
+    });
+    
+}
+function f2(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+        console.log("ABES College");
+        resolve();
+        },2000);
+    });
+}
+f1().then(f2)
+    .catch((err)=>{
+        console.log("Error");
+    });
+
+async function test() {
+    try{
+        await f1();
+        await f2();
+    }
+    catch(err){
+        console.log("error");
+    }
+}
+test();
